18b5d6fdc11d16107be328af0bd385d298e65640
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-03-19 18:05:56 +0000

    fixed our collections height and made instagram icon link to instagram

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,9 @@ export default function Home() {
         <img src="/candelalogo.svg" alt="logo" className="logoImage" />
         <div className="searchInstaBag">
           <IoSearch size = {50}/>
-          <FaInstagram size = {50}/>
+          <a href="https://www.instagram.com/_somoscandela/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram size = {50}/>
+          </a>
           <MdOutlineShoppingBag size = {50}/>
         </div>
       </div>
