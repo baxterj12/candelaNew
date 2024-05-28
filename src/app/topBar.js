5efd8ec300a19e5f3696a6fb00910c2f6bd65074
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-05-28 16:05:19 +0000

    added sidebar

--- src/app/topBar.js
+++ src/app/topBar.js
@@ -7,9 +7,14 @@ import { IoSearch } from "react-icons/io5";
 import { MdOutlineShoppingBag } from "react-icons/md";
 
 export default function TopBar() {
+    const [sidebarVisible, setSidebarVisible] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebarVisible(!sidebarVisible);
+    };
     return (
         <div className="topBar">
-            <FiAlignJustify size = {60} className="widen"/>
+            <FiAlignJustify size = {60} className="widen" onClick={toggleSidebar}/>
             <img src="/candelalogo.svg" alt="logo" className="logoImage" />
             <div className="searchInstaBag">
                 <IoSearch size = {50}/>
@@ -17,6 +22,15 @@ export default function TopBar() {
                     <FaInstagram size = {50}/>
                 </a>
                 <MdOutlineShoppingBag size = {50}/>
+            </div>
+            <div className={`sidebar ${sidebarVisible ? 'visible' : ''}`}>
+                <ul>
+                    <li><FiAlignJustify size = {60} onClick={toggleSidebar}/></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
             </div>
         </div>
     );
