9cf868bfcbc1e90aefbf96e33420ea3c5e3a2b31
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-05-28 16:13:17 +0000

    moved the bottom bar to its own file

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import react, {useState} from 'react';
 import './page.css';
 import TopBar from './topBar.js';
+import BottomBar from './bottomBar.js';
 import { FiAlignJustify } from "react-icons/fi";
 import { FaInstagram, FaTwitter, FaEnvelope } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
@@ -177,33 +178,7 @@ export default function Home() {
             </a>
           </div>
       </div>
-
-      <div className="contactInfo">
-        <div className="format">
-          <div className="getInTouch">
-            <h2>GET IN TOUCH</h2>
-            <div className="icons">
-              <FaTwitter size = {25}/>
-              <FaInstagram size = {25}/>
-              <FaEnvelope size={25} />
-              <p className="email">info@sequencecollection.com</p>
-            </div>
-          </div>
-          <div className="newsletter">
-            <h2>NEWSLETTER</h2>
-            <p>Sign up for the latest news, offers, and styles</p>
-            <div className = "input-group">
-              <input type="email" placeholder="Your email" />
-              <button type="submit">Subscribe</button>
-              </div>
-          </div>
-        </div>
-        <div className ="space">
-          
-        </div>
-        <p>@CopyRight © 2024, Sequence Collection. Ecommerce Software by Shopify</p>
-        <p>Design by GreySkyGraphics</p>
-      </div>
+      <BottomBar/>
     </div>
   );
 }
