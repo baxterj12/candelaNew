e335681bb5108b36efec3357932f6741ff4a7c18
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-05-30 18:40:47 +0100

    Update topBar.js

--- src/app/topBar.js
+++ src/app/topBar.js
@@ -16,7 +16,7 @@ export default function TopBar() {
     return (
         <div className="topBar">
             <FiAlignJustify size = {60}  onClick={toggleSidebar} style={{ marginLeft: '20px' }}/>
-            <img src="/candelalogo.svg" alt="logo" className="logoImage" />
+            <img src="../public/candelalogo.svg" alt="logo" className="logoImage" />
             <div className="searchInstaBag">
 
                 <a href="https://www.instagram.com/_somoscandela/" target="_blank" rel="noopener noreferrer">
