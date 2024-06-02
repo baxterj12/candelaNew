e78fc0fa68d2850528728e82049fca3fbdc3fdbc
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-06-02 21:20:01 +0000

    trying to fix images

--- src/app/page.js
+++ src/app/page.js
@@ -14,17 +14,17 @@ export default function Home() {
   const videoRef = useRef(null);
 
   const pantsPics = [
-    '/items/pancha/panchaPants/image1.svg',
-    '/items/pancha/panchaPants/image2.svg',
-    '/items/pancha/panchaPants/image3.svg',
-    '/items/pancha/panchaPants/image4.svg',
+    'items/pancha/panchaPants/image1.svg',
+    'items/pancha/panchaPants/image2.svg',
+    'items/pancha/panchaPants/image3.svg',
+    'items/pancha/panchaPants/image4.svg',
   ]
 
   const jacketPics = [
-    '/items/sanSebastian/sanSebastianJacket/image1.png',
-    '/items/sanSebastian/sanSebastianJacket/image2.svg',
-    '/items/sanSebastian/sanSebastianJacket/image3.svg',
-    '/items/sanSebastian/sanSebastianJacket/image4.svg',
+    'items/sanSebastian/sanSebastianJacket/image1.png',
+    'items/sanSebastian/sanSebastianJacket/image2.svg',
+    'items/sanSebastian/sanSebastianJacket/image3.svg',
+    'items/sanSebastian/sanSebastianJacket/image4.svg',
   ]
 
   const toggleMute = () => {
