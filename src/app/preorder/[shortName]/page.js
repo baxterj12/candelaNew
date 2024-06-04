1edf0b33110dcc76aa6a037a36d2e0dcffbf6fce
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-06-04 02:37:46 +0000

    added product page

--- src/app/preorder/[shortName]/page.js
+++ src/app/preorder/[shortName]/page.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import fs from 'fs';
 import path from 'path';
+import TopBar from '../../topBar.js';
+import './preorder.css';
 
 export async function generateStaticParams() {
   const filePath = path.join(process.cwd(), 'public', 'data', 'products.json');
@@ -17,9 +20,33 @@ export default function Preorder({ params }) {
   const jsonData = fs.readFileSync(filePath, 'utf-8');
   const products = JSON.parse(jsonData);
   const product = products.find(product => product.shortName === params.shortName);
+  for (let i = 0; i < product.images.length; i++) {
+    product.images[i] = `../../../${product.images[i]}`;
+  }
   return (
     <div>
-      <p>{product.desc}</p>
+      <TopBar/>
+      <div className="preorderContainer">
+        <div className="images">
+          <img src={product.images[1]} className="image1" />
+          <div className='imageCol'>
+            <img src={product.images[4]} className="imageInCol"/>
+            <img src={product.images[0]} className="imageInCol"/>
+          </div>
+        </div>
+        <div className="text">
+          <h1 className="name">{product.name}</h1>
+          <p>colors</p>
+          <p>sizes</p>
+          <p className="desc">{product.desc}</p>
+          <p className="button">Add to Cart</p>
+          <p className="disclaimer">Because our items are 100% handmade by artisans in 
+          El Salvador, our pre-order process takes between 4-6 weeks. Once you have 
+          placed your pre-order, we will updating you on the production process. This 
+          will allow you take a closer look at how we make our items while getting 
+          to know our brand better! </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/topBar.js
+++ src/app/topBar.js
@@ -16,7 +16,7 @@ export default function TopBar() {
     return (
         <div className="topBar">
             <FiAlignJustify size = {60}  onClick={toggleSidebar} style={{ marginLeft: '20px' }}/>
-            <img src="candelalogo.svg" alt="logo" className="logoImage"/>
+            <Link href="/"><img src="../candelaLogo.png" alt="logo" className="logoImage"/></Link>
             <div className="searchInstaBag">
 
                 <a href="https://www.instagram.com/_somoscandela/" target="_blank" rel="noopener noreferrer">
