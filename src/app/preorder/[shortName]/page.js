968394eefc7543c771e46935053fea733b3ba278
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-06-04 18:05:35 +0000

    made preorder its own doc so i can use client

--- src/app/preorder/[shortName]/page.js
+++ src/app/preorder/[shortName]/page.js
@@ -5,6 +5,7 @@ import TopBar from '../../topBar.js';
 import './preorder.css';
 import Circles from './circles.js'
 import SizeDrop from './sizeDrop.js'
+import Preorder from './preorder.js'
 
 export async function generateStaticParams() {
   const filePath = path.join(process.cwd(), 'public', 'data', 'products.json');
@@ -19,7 +20,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function Preorder({ params }) {
+export default function PreorderPage({ params }) {
 
   const filePath = path.join(process.cwd(), 'public', 'data', 'products.json');
   const jsonData = fs.readFileSync(filePath, 'utf-8');
@@ -32,28 +33,7 @@ export default function Preorder({ params }) {
     <div>
       <title>Candela | Preorder</title>
       <TopBar paramHelp="../"/>
-      <div className="preorderContainer">
-        <div className="images">
-          <img src={product.images[1]} className="image1" />
-          <div className='imageCol'>
-            <img src={product.images[4]} className="imageInCol"/>
-            <img src={product.images[0]} className="imageInCol"/>
-          </div>
-        </div>
-        <div className="text">
-          <h1 className="name">{product.name}</h1>
-          <p className="price">${product.price.toFixed(2)}</p>
-          <Circles colors={product.colors}/>
-          <SizeDrop sizes={product.sizes}/>
-          <p className="desc">{product.desc}</p>
-          <button type="submit" className="button">Add to Cart</button>
-          <p className="disclaimer">Because our items are 100% handmade by artisans in 
-          El Salvador, our pre-order process takes between 4-6 weeks. Once you have 
-          placed your pre-order, we will updating you on the production process. This 
-          will allow you take a closer look at how we make our items while getting 
-          to know our brand better! </p>
-        </div>
-      </div>
+      <Preorder product={product}/>
     </div>
   );
 }
