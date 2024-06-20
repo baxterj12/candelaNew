d98e046147dc254053f15b61efcfb456069ac501
baxterj12 <124473182+baxterj12@users.noreply.github.com>
2024-06-20 19:57:27 +0000

    added withcredentials: true

--- src/app/cart.js
+++ src/app/cart.js
@@ -49,7 +49,8 @@ export const CartProvider = ({ children }) => {
       const totalCost = cartItems.reduce((total, item) => total + item.product.price, 0);
       const items=stringifyCart()
       const response = await axios.post('/api',{cartItems: items, totalCost: totalCost},
-        { headers: {'Content-Type': 'application/json'}});
+        { headers: {'Content-Type': 'application/json'}},
+        { withCredentials: true });
       window.location.href = response.data.url;
     } catch (error) {
       console.error("error in cart async: ", error);
